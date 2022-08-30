786cc30585677d51210598a5a290a3bc75a89923
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-30 10:55:45 +0600

    home page update

--- src/components/Comment/DisplayComment.js
+++ src/components/Comment/DisplayComment.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './DisplayComment.css'
 
 const DisplayComment = (props) => {
-    const { name, rating, body } = props.comment
+    const { name, rating, body } = props.comment;
     return (
         <div className='comment'>
-            <h4>Name: {name.slice(0, 20)}</h4>
-            <h5>Rating: {rating} </h5>
-            <p>Comment: {body} </p>
+            <p><b>Name : </b>{name.slice(0, 20)}</p>
+            <p><b>Rating : </b>{rating}</p>
+            <p><b>Comments : </b>{body}</p>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,25 +4,24 @@ import useComments from "../../hooks/useComment";
 import DisplayComment from '../Comment/DisplayComment';
 import './Home.css'
 
-
 const Home = () => {
     const [comments] = useComments();
     const navigate = useNavigate()
 
     return (
         <div>
-            <div className='d-flex align-items-center body justify-content-evenly'>
+            <div className='d-flex align-items-center justify-content-evenly '>
                 <div>
                     <h2 className='fs-1 fw-bold'>Your Memories By Adorama</h2>
                     <p>For savings you won't find anywhere else and for weekly trending deals on top products from industry leading brands, browse Deals, Used, Deal Of The Day, only at Adorama.</p>
-                    <button className='btn-primary p-2 rounded-pill border-0'>Explore Now</button>
+                    <button className='btn-info text-white p-3 rounded border-0'>Explore Now</button>
                 </div>
-                <div>
+                <div className='body'>
                     <img src="https://images.unsplash.com/photo-1628285348080-38683c1232be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fGNhbWVyYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
                 </div>
             </div>
             <div>
-                <h2>Client Reviews</h2>
+                <h2 className='text-center my-5 text-info fw-bold '>Client Reviews</h2>
                 <div className='comment-area'>{
                     comments.slice(0, 3).map(comment => <DisplayComment
                         key={comment.id}
@@ -30,7 +29,9 @@ const Home = () => {
                     ></DisplayComment>)
                 }
                 </div>
-                <button onClick={() => navigate("/reviews")} className='btn-primary p-2 rounded-pill border-0' >See All Reviews</button>
+                <div className='text-center my-3'>
+                    <button onClick={() => navigate("/reviews")} className='btn-info p-3 rounded text-white text-center border-0' >See All Reviews</button>
+                </div>
             </div>
         </div>
     );
